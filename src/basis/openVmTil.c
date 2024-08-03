007e664e1ca99e0a9c737e35c7ef4a3e1de5ee28
Dennis Janssen <lovelightawareness@gmail.com>
2024-08-03 02:41:22 -0700

    a little cleanup

--- src/basis/openVmTil.c
+++ src/basis/openVmTil.c
@@ -1,6 +1,6 @@
 
 #include "../include/csl.h"
-#define VERSION ((byte*) "0.940.176" ) 
+#define VERSION ((byte*) "0.940.177" ) 
 
 // inspired by :: Foundations of Mathematical Logic [Foml] by Haskell Curry,
 // Category Theory, Object Oriented Programming, Type Theory 
